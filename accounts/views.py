7dd46f8be516491b6c5b4e17722b99b98a8c0ba2
Gregory <grzegorzgieda@gmail.com>
2019-02-06 14:01:29 +0100

    signup functionality added

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 def accounts(request):
-    return render(request, 'accounts.html')
+    return render(request, 'accounts/accounts.html')
 
 def logout(request):
-    return render(request, 'logout.html')
+    return render(request, 'accounts/logout.html')
 
 def login(request):
-    return render(request, 'login.html')
+    return render(request, 'accounts/login.html')
 
 def signup(request):
-    return render(request, 'signup.html')
+    if request.method == 'POST':
+        #User wants to sign up
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.get(username=request.POST['username'])
+                return render(request, 'accounts/signup.html', {'error':'Taki użytkownik już istnieje'})
+            except User.DoesNotExist:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['username'])
+                auth.login(request, user)
+                return redirect('home')
+        else:
+            return render(request, 'accounts/signup.html', {'error':'Podane hasła różnią się od siebie'})
+    else:
+        #User wants to enter info
+        return render(request, 'accounts/signup.html')
