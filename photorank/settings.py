cc51b0d7b2821af49d707de7fafd4bce8da8ab1c
Gregory <grzegorzgieda@gmail.com>
2019-02-07 22:22:06 +0100

    settings change

--- photorank/settings.py
+++ photorank/settings.py
@@ -12,16 +12,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
-try:
-    from .password import *
-except ImportError:
-    pass
-
-try:
-    from .local_settings import *
-except ImportError:
-    pass
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -142,3 +132,9 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+try:
+    from .password import *
+    from .local_settings import *
+except ImportError:
+    pass
